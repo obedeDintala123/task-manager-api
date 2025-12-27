0f3fb85251852567c740326238c3d197b7c2045e
obedeDintala123 <obededintala@gmail.com>
2025-12-27 14:00:02 +0100

    feat: add files

--- server.ts
+++ server.ts
@@ -23,6 +23,9 @@ server.use(
   })
 );
 
+server.get("/", (req, res) => {
+  res.send("API is running");
+});
 server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 server.use("/api/v1", userRouter);
 server.use("/api/v1/auth", authRouter);
